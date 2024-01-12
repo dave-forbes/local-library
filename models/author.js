3f569a6d7c29b7b73adfeeaaacda5233a3a4a346
David Forbes <davidforbesi@hotmail.com>
2024-01-12 17:44:50 +0000

    add date format virtual for form

--- models/author.js
+++ models/author.js
@@ -52,5 +52,13 @@ AuthorSchema.virtual("lifespan").get(function () {
   }
 });
 
+AuthorSchema.virtual("dob_yyyy_mm_dd").get(function () {
+  return DateTime.fromJSDate(this.date_of_birth).toISODate(); // format 'YYYY-MM-DD'
+});
+
+AuthorSchema.virtual("dod_yyyy_mm_dd").get(function () {
+  return DateTime.fromJSDate(this.date_of_death).toISODate(); // format 'YYYY-MM-DD'
+});
+
 // Export model
 module.exports = mongoose.model("Author", AuthorSchema);
